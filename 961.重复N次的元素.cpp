a18ceb5a2584894fcde5cbf20915e4a40b6d8f15
TankKingOne <1174389158@qq.com>
2019-10-04 20:25:15 +0800

    LeetCode-961

---
+++ 961.重复N次的元素.cpp
@@ -0,0 +1,17 @@
+//class Solution {
+//public:
+//	int repeatedNTimes(vector<int>& A) {
+//		size_t N = A.size() / 2;
+//		unordered_map<int, int> m;
+//		for (auto e : A)
+//		{
+//			m[e]++;
+//		}
+//		for (auto& e : m)
+//		{
+//			if (e.second == N)
+//				return e.first;
+//		}
+//		return 0;
+//	}
+//};
