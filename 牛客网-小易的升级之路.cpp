fe1ab8b2d8f3125f51c908fd79636b963eb2588c
TankKingOne <1174389158@qq.com>
2019-12-10 23:34:54 +0800

    牛客网-小易的升级之路

---
+++ 牛客网-小易的升级之路.cpp
@@ -0,0 +1,39 @@
+#includevector
+#includeiostream
+using namespace std;
+int GCD(int a, int b)
+{
+	int c;
+	while (c = a % b)
+	{
+		a = b;
+		b = c;
+	}
+	return b;
+}
+int main()
+{
+	int n, power;
+	vectorint num;
+	while (cin  n  power)
+	{
+		num.resize(n);
+		for (int i = 0; i n; i++)
+		{
+			cin  num[i];
+		}
+		for (int k = 0; k < n; k++)
+		{
+			if (num[k] < power)
+			{
+				power += num[k];
+			}
+			else
+			{
+				power += GCD(power, num[k]);
+			}
+		}
+		cout << power << endl;
+	}
+	return 0;
+}
