2e2c3b70642b1895c273721c224992e314dc9502
TankKingOne <1174389158@qq.com>
2020-03-29 22:57:47 +0800

    百度笔试-最大公约数和最小公倍数

---
+++ 百度-最大公约数和最小公倍数.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long int n;
+	cin >> n;
+	if (n == 1)
+		cout << 0 << endl;
+	else
+		cout << n * (n - 1) - 1 << endl;
+	return 0;
+}
